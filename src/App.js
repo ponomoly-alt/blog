0bd1741ee01ba267022313395ea87bb41386de52
ponomoly-alt <ponomoly1121@gmail.com>
2020-09-03 22:43:47 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import ReactMarkdown from 'react-markdown';
+import source from "./posts/2020-09-02.md"; 
 import './App.css';
 
+// const source = '# This is a header\n\nAnd this is a paragraph'
 function App() {
+  const [data, setData] = useState({post:null})
+  useEffect(() => {
+    fetch(source)
+      .then(res => res.text())
+      .then(post => {
+        console.log("post", post)
+        setData((state) => ({ ...state, post }))
+      })
+      .catch((err) => console.error(err));
+  }, [])
   return (
+    <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ReactMarkdown source={data.post} />
     </div>
+    </>
   );
 }
 
